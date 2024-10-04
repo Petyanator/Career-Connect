f45187842994b1a27f721da064c4fdd245cec8f0
Te El <vladislavpe4@gmail.com>
2024-10-04 13:49:08 +0900

    Update of the main branch

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\OneDrive\Рабочий стол\Career Connect\venv
+set VIRTUAL_ENV=C:\Users\User\Saved Games\Career Connect\Career-Connect\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
