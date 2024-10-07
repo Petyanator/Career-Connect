86457fcfae51def51579da23200b521e51e12bdd
Te El <vladislavpe4@gmail.com>
2024-10-07 15:38:53 +0900

    commit

--- server/app.py
+++ server/app.py
@@ -1,1 +1,21 @@
-print("Hello World")
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from config import Config
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+CORS(app, supports_credentials = True)
+bcrypt = Bcrypt(app)
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+jwt = JWTManager(app)
+
+from routes import routes 
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ server/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+# pip install python-dotenv
+#import redis
+
+load_dotenv()
+
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DATABASE")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    CORS_HEADERS = "Content-Type"
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
