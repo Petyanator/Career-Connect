3f985b7da4eef7f8dc0e3e865ba98f19c22d3eb7
Te El <vladislavpe4@gmail.com>
2024-10-08 13:56:07 +0900

    commit

--- server/app.py
+++ server/app.py
@@ -1,1 +1,21 @@
-print("Hello World")
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from config import Config, register_routes
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+CORS(app, supports_credentials = True)
+bcrypt = Bcrypt(app)
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+jwt = JWTManager(app)
+
+register_routes(app)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ server/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+# pip install python-dotenv
+#import redis
+
+load_dotenv()
+
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DATABASE")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    CORS_HEADERS = "Content-Type"
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+
+def register_routes(app):
+    routes_dir = os.path.join(os.path.dirname(__file__), "routes")
+    for filename in os.listdir(routes_dir):
+        if filename.endswith(".py") and not filename.startswith("__"):
+            module_name = f"routes.{filename[:-3]}"
+            __import__(module_name)
