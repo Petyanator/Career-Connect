ec2b53b9ff31a0a9b71b40a6d149082920b9099b
Daniel Chin <DanChin327@gmail.com>
2024-10-18 14:05:33 +0900

    fixed type on jobseekerprofileview importing jobseekerprofileview.css

--- client/src/pages/JobSeekerProfileView.jsx
+++ client/src/pages/JobSeekerProfileView.jsx
@@ -1,5 +1,5 @@
   import React, { useEffect } from 'react';
-  import './JobSeekerProfile.css'
+  import "./JobSeekerProfileView.css"
 
   function CreateProfileView({ profileData }) {
       useEffect(() => {
